5ee728f40b0fa7a4d988a19fc56b2124aecd8bb0
James Shields <james@lostcarpark.com>
2017-04-22 08:01:54 +0100

    Cache class started. Parked waiting answer to question.

---
+++ src/SimpleConregCache.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\simple_conreg\SimpleConregFieldOptions.
+ */
+
+namespace Drupal\simple_conreg;
+
+/**
+ * List options for Simple Convention Registration.
+ */
+class SimpleConregCache {
+
+  /**
+   * Remove the member list page from the cache so it will be rebuilt on next load.
+   */
+  public static function clearMemberList() {
+  }
+
+}
