5f569d880b4029440e94b101fbcbec4d3840c777
James Shields <james@lostcarpark.com>
2021-03-18 18:11:10 +0000

    Fix for missing 'array_key_first' function in PHP<7.3.

--- src/SimpleConregAdminMemberEdit.php
+++ src/SimpleConregAdminMemberEdit.php
@@ -22,6 +22,15 @@ use Drupal\node\NodeInterface;
 use Drupal\devel;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+// If PHP<7.3, add array_key_first function.
+if (!function_exists('array_key_first')) {
+    function array_key_first(array $arr) {
+        foreach($arr as $key => $unused) {
+            return $key;
+        }
+        return NULL;
+    }
+}
 
 /**
  * Simple form to add an entry, with all the interesting fields.
