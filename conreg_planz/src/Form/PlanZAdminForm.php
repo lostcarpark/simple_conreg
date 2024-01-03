38dbf6f045f09f09aa17e9b1db1240268c02a8f9
lostcarpark <james@lostcarpark.com>
2024-01-03 00:31:47 +0000

    Fill in missing class members.

--- conreg_planz/src/Form/PlanZAdminForm.php
+++ conreg_planz/src/Form/PlanZAdminForm.php
@@ -1,39 +1,31 @@
 <?php
-/**
- * @file
- * Contains \Drupal\conreg_planz\Form\PlanZAdminForm
- */
+
 namespace Drupal\conreg_planz\Form;
 
+use Drupal\conreg_planz\PlanZ;
+use Drupal\conreg_planz\PlanZUser;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\simple_conreg\FieldOptions;
 use Drupal\simple_conreg\Member;
-use Drupal\conreg_planz\PlanZ;
-use Drupal\conreg_planz\PlanZUser;
-
-use Drupal\devel;
 
 /**
  * Configure simple_conreg settings for this site.
  */
-class PlanZAdminForm extends FormBase
-{
+class PlanZAdminForm extends FormBase {
   private PlanZ $planz;
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId()
-  {
+  public function getFormId() {
     return 'conreg_planz_admin';
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames()
-  {
+  protected function getEditableConfigNames() {
     return [
       'conreg_planz.admin',
     ];
@@ -42,86 +34,84 @@ class PlanZAdminForm extends FormBase
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, $eid = 1)
-  {
+  public function buildForm(array $form, FormStateInterface $form_state, $eid = 1) {
     // Store Event ID in form state.
     $form_state->set('eid', $eid);
 
-    $config = \Drupal::config('simple_conreg.settings.'.$eid.'.planz');
+    $config = \Drupal::config('simple_conreg.settings.' . $eid . '.planz');
     $this->planz = new PlanZ($config);
     /**
      * Manual member invites.
      */
 
-    $form['info'] = array(
+    $form['info'] = [
       '#type' => 'markup',
       '#prefix' => '<div class="email_members">',
       '#suffix' => '</div>',
       '#markup' => $this->t('Manually add members and send invite emails.'),
-    );
+    ];
 
-    $form['member_search'] = array(
+    $form['member_search'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Search for member'),
-    );
+    ];
 
     $form['search_button'] = [
       '#type' => 'button',
       '#value' => $this->t('Search'),
       '#ajax' => [
         'wrapper' => 'search-results',
-        'callback' => array($this, 'callbackSearch'),
+        'callback' => [$this, 'callbackSearch'],
         'event' => 'click',
       ],
     ];
 
-    $form['search_results'] = array(
+    $form['search_results'] = [
       '#type' => 'markup',
       '#prefix' => '<div id="search-results">',
       '#suffix' => '</div>',
-    );
+    ];
 
-    $form['member_range'] = array(
+    $form['member_range'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Member Number Range to Invite'),
-      '#description' => $this->t('Enter Member Nos to invite. Use commas (,) to separate ranges and hyphens (-) to separate range limits, e.g. "1,3,5-7".')
-    );
+      '#description' => $this->t('Enter Member Nos to invite. Use commas (,) to separate ranges and hyphens (-) to separate range limits, e.g. "1,3,5-7".'),
+    ];
 
-    $form['override'] = array(
+    $form['override'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Override member options - all members in range will be added'),
-    );
+    ];
 
-    $form['reset'] = array(
+    $form['reset'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Reset passwords for existing members - if checked will update password of members already existing on PlanZ with new random ones (note, we recommend getting users to use password reset on PlanZ rather than setting password centrally)'),
-    );
+    ];
 
-    $form['dont_email'] = array(
+    $form['dont_email'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Don\'t email - if checked no emails will be sent, member info will display on page. Only use for members who have difficulty receiving emails.'),
-    );
+    ];
 
     $form['manual_add'] = [
       '#type' => 'button',
       '#value' => $this->t('Manual Add to PlanZ'),
       '#ajax' => [
         'wrapper' => 'manual-add-result',
-        'callback' => array($this, 'callbackManualAdd'),
+        'callback' => [$this, 'callbackManualAdd'],
         'event' => 'click',
       ],
     ];
 
-    $form['result'] = array(
+    $form['result'] = [
       '#type' => 'markup',
       '#prefix' => '<div id="manual-add-result">',
       '#suffix' => '</div>',
       '#markup' => $this->t('Results will go here.'),
-    );
+    ];
 
     return $form;
   }
-
 
   /**
    * {@inheritdoc}
@@ -132,8 +122,9 @@ class PlanZAdminForm extends FormBase
 
   /**
    * Callback for member search. Search ConReg members and check if present on PlanZ/PlanZ.
+   *
    * @param array $form
-   * @param FormStateInterface $form_state
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
    */
   public function callbackSearch(array $form, FormStateInterface $form_state) {
     $vals = $form_state->getValues();
@@ -158,10 +149,11 @@ class PlanZAdminForm extends FormBase
     $select->condition('m.eid', $eid);
     $select->condition('m.is_paid', 1);
     $select->condition('m.is_approved', 1);
-    $select->condition("is_deleted", FALSE); //Only include members who aren't deleted.
+    // Only include members who aren't deleted.
+    $select->condition("is_deleted", FALSE);
     foreach (explode(' ', $vals['member_search']) as $word) {
       // Escape search word to prevent dangerous characters.
-      $esc_word = '%'.$connection->escapeLike($word).'%';
+      $esc_word = '%' . $connection->escapeLike($word) . '%';
       $likes = $select->orConditionGroup()
         ->condition('m.first_name', $esc_word, 'LIKE')
         ->condition('m.last_name', $esc_word, 'LIKE')
@@ -171,49 +163,50 @@ class PlanZAdminForm extends FormBase
     }
     $select->orderBy('m.member_no');
     // Make sure we only get items 0-49, for scalability reasons.
-    //$select->range(0, 50);
-
+    // $select->range(0, 50);.
     $entries = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
 
-    $rows = array();
-    $headers = array(
+    $rows = [];
+    $headers = [
       $this->t('Member No'),
       $this->t('First Name'),
       $this->t('Last Name'),
       $this->t('email'),
       $this->t('PlanZ Badge ID'),
-    );
+    ];
 
     foreach ($entries as $entry) {
       // Sanitize each entry.
       $rows[] = array_map('Drupal\Component\Utility\Html::escape', (array) $entry);
     }
 
-    $form['search_results']['table'] = array(
+    $form['search_results']['table'] = [
       '#type' => 'table',
       '#header' => $headers,
       '#rows' => $rows,
       '#empty' => $this->t('No entries available.'),
       '#sticky' => TRUE,
-    );
+    ];
 
     return $form['search_results'];
   }
 
   /**
    * Callback for manual add button. Add specified members to PlanZ/PlanZ.
+   *
    * @param array $form
-   * @param FormStateInterface $form_state
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
    * @return array The updated "Result" form element.
    */
   public function callbackManualAdd(array $form, FormStateInterface $form_state) {
     $eid = $form_state->get('eid');
-    $config = \Drupal::config('simple_conreg.settings.'.$eid.'.planz');
+    $config = \Drupal::config('simple_conreg.settings.' . $eid . '.planz');
 
     $fieldOptions = FieldOptions::getFieldOptions($eid);
     $optionFields = [];
     foreach ($fieldOptions->options as $option) {
-      if ($config->get('option_fields.'.$option->optionId) ?: FALSE) {
+      if ($config->get('option_fields.' . $option->optionId) ?: FALSE) {
         $optionFields[$option->optionId] = TRUE;
       }
     }
@@ -223,7 +216,7 @@ class PlanZAdminForm extends FormBase
     if (preg_match('/^([0-9]+(\-[0-9]+)?,)*[0-9]+(\-[0-9]+)?$/', $vals['member_range']) == 1) {
       foreach (explode(',', $vals['member_range']) as $range) {
         $output[] = "<p>$range</p>";
-        list($min, $max) = array_pad(explode('-', $range), 2, '');
+        [$min, $max] = array_pad(explode('-', $range), 2, '');
         if (empty($max)) {
           // If no max set, range is single number in min.
           $output[] = $this->addMemberToPlanZ($eid, $min, $vals['override'], $vals['reset'], $vals['dont_email'], $optionFields);
@@ -233,8 +226,8 @@ class PlanZAdminForm extends FormBase
             $output[] = $this->addMemberToPlanZ($eid, $num, $vals['override'], $vals['reset'], $vals['dont_email'], $optionFields);
           }
         }
-      // Log an event to show a member check occurred.
-      //\Drupal::logger('conreg_planz')->info("Manual Add pressed.");
+        // Log an event to show a member check occurred.
+        // \Drupal::logger('conreg_planz')->info("Manual Add pressed.");.
       }
       $form['result']['#markup'] = implode("\n", $output);
     }
@@ -247,16 +240,22 @@ class PlanZAdminForm extends FormBase
 
   /**
    * Add a member to the PlanZ/PlanZ database.
-   * @param int $eid The event ID
-   * @param int $memberNo The member number being added
-   * @param boolean $override If true, add member even if they don't meet criteria
-   * @param boolean $reset If true, reset the user's password
-   * @param boolean $dontEmail If true, don't send email notification
+   *
+   * @param int $eid
+   *   The event ID.
+   * @param int $memberNo
+   *   The member number being added.
+   * @param bool $override
+   *   If true, add member even if they don't meet criteria.
+   * @param bool $reset
+   *   If true, reset the user's password.
+   * @param bool $dontEmail
+   *   If true, don't send email notification.
    * @param array $optionFields
+   *
    * @return string Returns the details of the newly added member.
    */
-  private function addMemberToPlanZ(int $eid, int $memberNo, bool $override, bool $reset, bool $dontEmail, array $optionFields): string
-  {
+  private function addMemberToPlanZ(int $eid, int $memberNo, bool $override, bool $reset, bool $dontEmail, array $optionFields): string {
     $member = Member::loadMemberByMemberNo($eid, $memberNo);
     if (is_null($member)) {
       return "<p>Member: $memberNo does not exist.</p>";
@@ -280,15 +279,17 @@ class PlanZAdminForm extends FormBase
         $this->planz->sendInviteEmail($user);
       }
 
-      return $this->t('<p>Member: @first_name @last_name<br />'.
-                      'Badge id: @badgeid.<br />'.
-                      'Password: @password<br />'.
+      return $this->t('<p>Member: @first_name @last_name<br />' .
+                      'Badge id: @badgeid.<br />' .
+                      'Password: @password<br />' .
                       'URL: @url</p>',
-                      ['@first_name' => $member->first_name,
-                       '@last_name' => $member->last_name,
-                       '@badgeid' => $user->badgeId,
-                       '@password' => $user->password ?? '',
-                       '@url' => $this->planz->planZUrl]);
+                      [
+                        '@first_name' => $member->first_name,
+                        '@last_name' => $member->last_name,
+                        '@badgeid' => $user->badgeId,
+                        '@password' => $user->password ?? '',
+                        '@url' => $this->planz->planZUrl,
+                      ]);
     }
 
     // If this point reached, member not added, so return empty string.
--- conreg_planz/src/PlanZUser.php
+++ conreg_planz/src/PlanZUser.php
@@ -1,41 +1,41 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\conreg_planz\PlanZUser.
- */
-
 namespace Drupal\conreg_planz;
 
 use Drupal\Core\Database\Connection;
 use Drupal\simple_conreg\Member;
-use Drupal\devel;
 
-class PlanZUser
-{
+/**
+ *
+ */
+class PlanZUser {
   private PlanZ $planz;
   public int $mid;
   public string $badgeId;
   public ?string $password;
   public ?string $hashedPassword;
+  public bool $existingParticipant = FALSE;
+  public ?string $pubsname = NULL;
+  public ?string $sortedpubsname = NULL;
 
   /**
    * Constructs a new Member object.
    */
-  public function __construct(PlanZ $planz)
-  {
+  public function __construct(PlanZ $planz) {
     $this->planz = $planz;
-    $this->password = null;
-    $this->hashedPassword = null;
+    $this->password = NULL;
+    $this->hashedPassword = NULL;
   }
-  
+
   /**
    * Load member PlanZ link info.
-   * @param int $mid Member ID
+   *
+   * @param int $mid
+   *   Member ID.
+   *
    * @return bool TRUE if member details found. FALSE if member not present on PlanZ.
    */
-  public function load(int $mid): bool
-  {
+  public function load(int $mid): bool {
     // Get regular Drupal DB connection.
     $connection = \Drupal::database();
     $select = $connection->select('conreg_planz', 'z');
@@ -49,7 +49,7 @@ class PlanZUser
 
     $this->mid = $mid;
     $this->badgeId = $badgeId;
-    
+
     // Get the PlanZ connection to get the Participant table.
     $planZCon = $this->planz->getPlanZConnection();
     $select = $planZCon->select('Participants', 'P');
@@ -72,11 +72,13 @@ class PlanZUser
 
   /**
    * Check if member is an existing PlanZ user. If found, create on conreg_planz table.
-   * @param int $mid Member ID
+   *
+   * @param int $mid
+   *   Member ID.
+   *
    * @return bool TRUE if member exists on PlanZ
    */
-  private function checkExistingUser(int $mid): bool
-  {
+  private function checkExistingUser(int $mid): bool {
     // Get member details.
     $member = Member::loadMember($mid);
 
@@ -89,7 +91,7 @@ class PlanZUser
 
     // If false returned, member is not PlanZ user.
     if (!$badgeId) {
-      return false;
+      return FALSE;
     }
 
     // Check that the user we've found isn't already linked to a different member.
@@ -102,7 +104,7 @@ class PlanZUser
 
     // If member found, user already belongs to another member.
     if ($foundMid) {
-      return false;
+      return FALSE;
     }
 
     // We've found a PlanZ user, and confirmed it's not assigned to another ConReg member, so update member badge ID and save to conreg_planz.
@@ -110,23 +112,24 @@ class PlanZUser
     $this->badgeId = $badgeId;
     $this->saveConregPlanZ();
     // Member is linked to a PlanZ user so return true.
-    return true;
+    return TRUE;
   }
 
   /**
    * Save ConReg member to PlanZ.
-   * @param Member $member The member object to save
-   * @param bool $reset If true, reset member password.
-   * 
+   *
+   * @param \Drupal\simple_conreg\Member $member
+   *   The member object to save.
+   * @param bool $reset
+   *   If true, reset member password.
    */
-  public function save(Member $member, bool $reset = FALSE)
-  {
+  public function save(Member $member, bool $reset = FALSE) {
     $this->mid = $member->mid;
-    
+
     if ($reset) {
       $this->hashedPassword = NULL;
     }
-    
+
     if (empty($this->badgeId)) {
       // Member does not have badge ID, so need to create one, and save to conreg_planz table.
       $this->badgeId = $this->planz->createBadgeId($member);
@@ -134,10 +137,10 @@ class PlanZUser
     }
 
     $planZCon = $this->planz->getPlanZConnection();
-    
+
     // Always save latest member details to PlanZ.
     $this->saveCongoDump($planZCon, $member);
-    
+
     // Only save other tables if password not already set.
     if (empty($this->hashedPassword)) {
       // To do: make other options for participant name available.
@@ -157,8 +160,7 @@ class PlanZUser
   /**
    * Save the PlanZ badge ID.
    */
-  private function saveConregPlanZ(): void
-  {
+  private function saveConregPlanZ(): void {
     $connection = \Drupal::database();
     $connection->upsert('conreg_planz')
       ->fields(['mid' => $this->mid, 'badgeid' => $this->badgeId, 'update_date' => time()])
@@ -168,50 +170,57 @@ class PlanZUser
 
   /**
    * Save member to CongoDump table.
-   * @param Connection $planZCon Connection to PlanZ database
-   * @param Member $member The member to save
+   *
+   * @param \Drupal\Core\Database\Connection $planZCon
+   *   Connection to PlanZ database.
+   * @param \Drupal\simple_conreg\Member $member
+   *   The member to save.
+   *
    * @return int Number of rows affected (usually 1)
    */
-  private function saveCongoDump(Connection $planZCon, Member $member): int
-  {
+  private function saveCongoDump(Connection $planZCon, Member $member): int {
     $return_value = $planZCon->upsert('CongoDump')
-        ->fields([
-                'badgeid' => $this->badgeId,
-                'firstname' => substr($member->first_name, 0, 30),
-                'lastname' => substr($member->last_name, 0, 40),
-                'badgename' => substr($member->badge_name, 0, 50),
-                'phone' => substr($member->phone, 0, 100),
-                'email' => substr($member->email, 0, 100),
-                'postaddress1' => substr($member->street, 0, 100),
-                'postaddress2' => substr($member->street2, 0, 100),
-                'postcity' => substr($member->city, 0, 50),
-                'poststate' => substr($member->county, 0, 25),
-                'postzip' => substr($member->postcode, 0, 10),
-                'postcountry' => substr($member->country, 0, 25),
-                'regtype' => $member->badge_type,
-                ])
-        ->key('badgeid')
-        ->execute();  
+      ->fields([
+        'badgeid' => $this->badgeId,
+        'firstname' => substr($member->first_name, 0, 30),
+        'lastname' => substr($member->last_name, 0, 40),
+        'badgename' => substr($member->badge_name, 0, 50),
+        'phone' => substr($member->phone, 0, 100),
+        'email' => substr($member->email, 0, 100),
+        'postaddress1' => substr($member->street, 0, 100),
+        'postaddress2' => substr($member->street2, 0, 100),
+        'postcity' => substr($member->city, 0, 50),
+        'poststate' => substr($member->county, 0, 25),
+        'postzip' => substr($member->postcode, 0, 10),
+        'postcountry' => substr($member->country, 0, 25),
+        'regtype' => $member->badge_type,
+      ])
+      ->key('badgeid')
+      ->execute();
 
     return $return_value;
   }
 
   /**
-   * Save to the Participants table
-   * @param Connection $planZCon The connection to the PlanZ database
-   * @param string $publicationName The name to save to the pubsname field
-   * @param string $sortingName The name to save to the sortedpubsname field
+   * Save to the Participants table.
+   *
+   * @param \Drupal\Core\Database\Connection $planZCon
+   *   The connection to the PlanZ database.
+   * @param string $publicationName
+   *   The name to save to the pubsname field.
+   * @param string $sortingName
+   *   The name to save to the sortedpubsname field.
+   *
    * @return int Number of rows affected (normally 1)
    */
-  private function saveParticipant(Connection $planZCon, string $publicationName = NULL, string $sortingName = NULL): int
-  {
+  private function saveParticipant(Connection $planZCon, string $publicationName = NULL, string $sortingName = NULL): int {
     if ($this->planz->generatePassword && empty($this->hashedPassword)) {
       $this->password = $this->generatePassword(12);
       $this->hashedPassword = password_hash(trim($this->password), PASSWORD_DEFAULT);
     }
 
     $fields = [
-      'badgeid' => $this->badgeId
+      'badgeid' => $this->badgeId,
     ];
 
     if (empty($this->existingParticipant)) {
@@ -225,7 +234,7 @@ class PlanZUser
     if (!empty($this->hashedPassword)) {
       $fields['password'] = $this->hashedPassword;
     }
-    
+
     // Only update publication name if none already set.
     if (empty($this->pubsname) && !empty($publicationName)) {
       $fields['pubsname'] = $publicationName;
@@ -234,43 +243,49 @@ class PlanZUser
     if (empty($this->sortedpubsname) && !empty($sortingName)) {
       $fields['sortedpubsname'] = $sortingName;
     }
-     
+
     $return_value = $planZCon->upsert('Participants')
-        ->fields($fields)
-        ->key('badgeid')
-        ->execute();
-        
+      ->fields($fields)
+      ->key('badgeid')
+      ->execute();
+
     return $return_value;
   }
 
   /**
    * Save a user role to the UserHasPermissionRole table.
-   * @param Connection $planZCon Connection to PlanZ database
-   * @param int $role The ID of the role to add
+   *
+   * @param \Drupal\Core\Database\Connection $planZCon
+   *   Connection to PlanZ database.
+   * @param int $role
+   *   The ID of the role to add.
+   *
    * @return int Number of rows updated
    */
-  private function saveUserRole(Connection $planZCon, int $role): int
-  {
+  private function saveUserRole(Connection $planZCon, int $role): int {
 
     $return_value = $planZCon->upsert('UserHasPermissionRole')
-        ->fields([
-                'badgeid' => $this->badgeId,
-                'permroleid' => $role,
-                ])
-        ->key('badgeid')
-        ->execute();  
+      ->fields([
+        'badgeid' => $this->badgeId,
+        'permroleid' => $role,
+      ])
+      ->key('badgeid')
+      ->execute();
 
     return $return_value;
   }
 
   /**
-   * Generate a random password of specified length
-   * @param int $chars Number of characters length for password
+   * Generate a random password of specified length.
+   *
+   * @param int $chars
+   *   Number of characters length for password.
+   *
    * @return string A random password
    */
-  function generatePassword(int $chars): string
-  {
+  public function generatePassword(int $chars): string {
     $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz!%^*()[]{};:@#,/';
     return substr(str_shuffle($data), 0, $chars);
   }
+
 }
